27de845444c2b709ca23f32ba626badf6af2c9bf
slasurkar <sandeepl@xpanxion.co.in>
2014-04-25 17:11:57 +0530

    Created Initial Applicatio Structure

---
+++ Omaha/Omaha.Web/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Omaha.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Omaha/Omaha.Web/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Omaha.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
