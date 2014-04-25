57ec3b0b5c99db7be2dee4579486ee3e0af740b8
slasurkar <sandeepl@xpanxion.co.in>
2014-04-25 19:39:43 +0530

    Created Initial Project Structure

---
+++ Crawford.Omaha/Crawford.Omaha.Web/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Crawford.Omaha.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Crawford.Omaha/Crawford.Omaha.Web/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace Crawford.Omaha.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
